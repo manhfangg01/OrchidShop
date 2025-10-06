647da1fdf3ebdb3295d3627a10b38519d794925e
PhanVanManh <phanvanmanh3936@gmail.com>
2025-10-06 14:34:15 +0700

    initialize vite frontend and spring backend

---
+++ orchid-backend/src/main/java/com/orchid_backend/OrchidBackendApplication.java
@@ -0,0 +1,13 @@
+package com.orchid_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchidBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchidBackendApplication.class, args);
+	}
+
+}
---
+++ orchid-backend/src/test/java/com/orchid_backend/OrchidBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orchid_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchidBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
